469a87915d198fed47fdc126d9489ac2ae95b915
Maic Siemering <maic@siemering.tech>
2025-04-25 13:19:57 +0200

    Update gridTransformer.ts
    allow grid values (drag and drop) to be floats (eg 7.5)

--- src/transformers/gridTransformer.ts
+++ src/transformers/gridTransformer.ts
@@ -13,7 +13,7 @@ export class GridTransformer implements AttributeTransformer {
 	}
 
 	private gridAttributeRegex =
-		/^(?:(-?\d*(?:px)?)(?:\s*|x)(-?\d*(?:px)?)|(center|top|bottom|left|right|topleft|topright|bottomleft|bottomright))$/m;
+		/^(?:(-?(?:\d+(?:\.\d*)?|\.\d+)(?:px)?)(?:\s*|x)(-?(?:\d+(?:\.\d*)?|\.\d+)(?:px)?))|(center|top|bottom|left|right|topleft|topright|bottomleft|bottomright))$/m;
 
 	transform(element: Properties) {
 
